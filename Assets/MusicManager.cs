8bda1b28cdcdb76cd79888058258cac8d72810f9
Patrick Bardo <dr.p.bardo@gmail.com>
2017-10-03 23:57:21 +0200

    Added the Scenes for the rest of the project

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = levelMusicChangeArray[0];
+		audioSource.loop = true;
+		audioSource.Play();
+	}
+
+	void OnEnable()
+     {
+      //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+         SceneManager.sceneLoaded += OnLevelFinishedLoading;
+     }
+         
+     void OnDisable()
+     {
+     //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+         SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+     }
+ 
+     void OnLevelFinishedLoading (Scene scene, LoadSceneMode mode)
+	{
+		Debug.Log ("Level Loaded: " + scene.buildIndex);
+
+		if (levelMusicChangeArray [scene.buildIndex]) {
+			audioSource.clip = levelMusicChangeArray [scene.buildIndex];
+			audioSource.loop = true;
+			audioSource.Play ();
+		} else {
+			Debug.Log("Missing AudioClip at index: " + scene.buildIndex);
+		}
+
+     }
+}
